12d8c11098ce2bdd89cdc52126abc4e26d37d9cf
Luccas Rocigno de medeiros <lrocigno@c1r3p1.42sp.org.br>
2021-03-07 00:32:13 -0300

    nothing much

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,14 @@
 /*   By: lrocigno <lrocigno@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/05 10:12:34 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/03/05 10:23:26 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/03/06 21:56:28 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
+
+# include <unistd.h>
 
 int	get_next_line(int fd, char **line);
 
